a0b6b637f03c424f4738ea029165ab720f13f80d
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-03-03 20:23:34 -0500

    Third commit. Justin Rittenhouse is the GOAT

--- src/view/Inbox.js
+++ src/view/Inbox.js
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom'
 import { DataContext } from "../contexts/DataProvider"
 import { firebaseApp } from '../firebase/config';
 import moment from 'moment'
+import { MessageList } from '../components/MessageList'
+import { getFirestore } from 'firebase/firestore';
 
 
 export const Inbox = ( props ) => {
 
   const p = props.user
   const { messages } = useContext( DataContext )
+  const db = getFirestore()
 
   return (
       <React.Fragment>
-          { messages.map(p) }
+        <h2> Your Incoming Messages </h2>
+        <MessageList messages={ messages } />
       </React.Fragment>
   )
 }
